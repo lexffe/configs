dde03e83d0c4577291f1a84565c77e4ec031c467
Alex V <2038611+lexffe@users.noreply.github.com>
2021-01-30 15:07:50 +0000

    update/fix nix config

--- nixos-minimal-vm.nix
+++ nixos-minimal-vm.nix
@@ -1,11 +1,11 @@
 # very basic config for bootstrapping a workable nixos environment (VM)
-
-{ config, pkgs, ... };
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -14,8 +14,10 @@
 
   time.timeZone = "Europe/London";
 
-  ## iface names differ.
-  # networking.interfaces.ens160.useDHCP = true;
+  networking.useDHCP = false;
+  networking.interfaces.ens160.useDHCP = true;
+
+  # i18n.defaultLocale = "en_US.UTF-8";
 
   services.printing.enable = false;
 
@@ -24,23 +26,38 @@
 
   users.users.asm = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    shell = pkgs.zsh
-  }
+    extraGroups = [ "wheel" ]; 
+    shell = pkgs.zsh;
+  };
 
   environment.systemPackages = with pkgs; [
-    wget vim emacs
-    open-vm-tools ## vmware
+    wget vim zsh git
+    open-vm-tools
   ];
 
   programs.zsh.enable = true;
 
-  ## !!!
+  ## !!! Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   ## !!!
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
-  system.stateVersion = "20.09";
+  system.stateVersion = "21.03";
+
+  hardware.opengl.enable = true;
+  programs.xwayland.enable = true;
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome3.enable = true;
+    desktopManager.default = "gnome3";
+    displayManager.gdm.wayland = true;
+  };
+
 }
-  
